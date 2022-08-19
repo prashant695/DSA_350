98aef65cf6c04bb9e3aaac61b3274282cf1e71d1
Prashant Agarwal <88228831+prashant695@users.noreply.github.com>
2022-08-19 10:15:54 +0530

    Update 6. Intersection of two linked list.cpp

--- Linked List/6. Intersection of two linked list.cpp
+++ Linked List/6. Intersection of two linked list.cpp
@@ -54,3 +54,30 @@ public:
 // Count nodes in listA and listB
 // If nodes in listA > listB move head in listA by (nodes in A - nodes in B) else vice versa
 // Move both the heads parallely and check for the current node value
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+{
+    ListNode *p = headA, *q = headB;
+    while (p and q)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    while (p)
+    {
+        headA = headA->next;
+        p = p->next;
+    }
+
+    while (q)
+    {
+        headB = headB->next;
+        q = q->next;
+    }
+    while (headA and headB and headA != headB)
+    {
+        headA = headA->next;
+        headB = headB->next;
+    }
+    return headA;
+}
